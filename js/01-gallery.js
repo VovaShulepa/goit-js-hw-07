1e5fb2163dd789b3c2647bbde2466da9326c0f20
Shulepa Volodymyr <shulepatrader@gmail.com>
2022-11-07 01:12:34 +0200

    complete 2 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+// Gallery
+
+const divEl = document.querySelector(".gallery");
+
+function createGalleryItems(items) {
+  return items
+    .map(
+      (item) => ` <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+         <img
+         class="gallery__image"
+         src="${item.preview}"
+         data-source="${item.original}"
+         alt="${item.description}"/>
+        </a>
+    </div>`
+    )
+    .join("");
+}
+
+const addGalleryItems = createGalleryItems(galleryItems);
+
+divEl.innerHTML = addGalleryItems;
+divEl.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  // Close the lightbox
+  divEl.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const createGalleryItems = () => {
+  const img = galleryItems
+    .map(
+      (item) => ` 
+        <a class="gallery__item" href="${item.original}">
+         <img
+         class="gallery__image"
+         src="${item.preview}"
+         alt="${item.description}"/>
+        </a>`
+    )
+    .join("");
+  galleryEl.innerHTML = img;
+};
+
+createGalleryItems();
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionData: "alt",
+  captionDelay: 9250,
+});
